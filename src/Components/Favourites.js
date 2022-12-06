3d110e44ff62206025851052d5e5fe7b74705e4b
ashish <kumarashish2615@gmail.com>
2022-12-06 13:46:52 +0530

    added clickable image in favorites and fixed bugs in favorites tab

--- src/Components/Favourites.js
+++ src/Components/Favourites.js
@@ -1,39 +1,59 @@
 import React, { Component } from 'react'
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import axios from 'axios'
-import {genre_ids,table_headers} from './FavoritesHelper'
+import {genre_hash, table_headers} from './FavoritesHelper'
+import {Link} from 'react-router-dom'
 
 export default class Favourites extends Component {
   constructor(){
     super();
     this.state = {
-      movies: [],
+      movies: JSON.parse(localStorage.getItem('favourites') == null
+              ? "[]"
+              : localStorage.getItem('favourites')),
       genres: [],
       curr_genre: 'all_genres'
-    };
+    }
   }
 
   async componentDidMount(){
-    let res = await axios.get(`${process.env.REACT_APP_HEADER}/3/trending/movie/day?api_key=${process.env.REACT_APP_API_KEY}&page=1`);
-    let data = res.data
-    this.setState({
-      movies:[...data.results],
-      genres: [...this.get_genre(data.results)]
-    })
+    this.get_genre(this.state.movies);
   }
 
   get_genre = (movies) => {
-    let unique_ids = []
-    movies.map((movie)=>{
-      return(
-      movie.genre_ids.forEach((id)=>{
-        if (!unique_ids.includes(id))
-          unique_ids.push(id)
-      })
-    )})
-    return unique_ids
+    movies.forEach((movie) =>
+      movie.genre_ids ? this.add_to_genre(movie.genre_ids) : this.add_to_genre(movie.genres,true))
+
+      console.log(this.state.movies,this.state.genres)
   }
+
+  add_to_genre = (ids, is_hash = false)=>{
+    if(is_hash)
+    {
+      ids.map((id)=>{
+        let curr_data = this.state.genres
+        curr_data.sort()
+        if(!curr_data.includes(id.name))
+          {
+            curr_data.push(id.name)
+            this.setState({genres: [...curr_data]})
+          }
+      })
+    }
+    else
+    {
+      ids.map((id)=>{
+        let curr_data = this.state.genres
+        if(!curr_data.includes(genre_hash[id]))
+          {
+            curr_data.push(genre_hash[id])
+            curr_data.sort()
+            this.setState({genres: [...curr_data]})
+          }
+      })
+    }
+  }
+
 
   update_genre = (e)=>{
     console.log(e.target.id)
@@ -57,15 +77,16 @@ export default class Favourites extends Component {
                 </li>
               }
               {
-                this.state.genres.map((genre,index)=>{
+                this.state.genres.map((genre,index) => {
                   return(
-                    this.state.curr_genre === genre_ids[genre] ?
-                      <li className="nav-item ">
-                        <p className="nav-link btn selected-genre"> {genre_ids[genre]}</p>
+                    this.state.curr_genre === genre
+                    ?
+                      <li className="nav-item" key = {index}>
+                        <p className="nav-link btn selected-genre">{genre}</p>
                       </li>
-                      :
-                      <li className="nav-item ">
-                        <p className="nav-link btn genre-btn" onClick={()=>{this.setState({curr_genre: genre_ids[genre]})}}>{genre_ids[genre]}</p>
+                    :
+                      <li className="nav-item" key = {index}>
+                        <p className="nav-link btn genre-btn" onClick={()=>{this.setState({curr_genre: genre})}}>{genre}</p>
                       </li>
                   )
                 })
@@ -96,12 +117,36 @@ export default class Favourites extends Component {
                     this.state.movies.map((movie,index) =>{
                       return (
                         <tr key = {index}>
-                          <td><img src = {`${process.env.REACT_APP_API_POSTER_PATH}${movie.backdrop_path}`}alt = ""
-                          className = "fav-image"/></td>
-
+                          <td>
+                            <Link to= {{
+                              pathname: `/movie/${movie.id}`,
+                                }}>
+                              <img src = {`${process.env.REACT_APP_API_POSTER_PATH}${movie.backdrop_path}`}alt = ""
+                              className = "fav-image"/>
+                            </Link>
+                          </td>
                           <td>{movie.title || movie.name}</td>
-                          <td>{movie.genre_ids.map((id,index)=>{return(index === movie.genre_ids.length - 1 ?
-                          genre_ids[id] : genre_ids[id]+",")})}</td>
+                          <td>
+                            {
+                              movie.genres
+                              ?
+                              movie.genres.map((genre,index) => {
+                                return(
+                                  index === (movie.genres.length - 1)
+                                  ? genre.name
+                                  : genre.name+","
+                                )
+                              })
+                              :
+                              movie.genre_ids.map((genre,index) => {
+                                return(
+                                  index === (movie.genre_ids.length - 1)
+                                  ? genre_hash[genre]
+                                  : genre_hash[genre]+","
+                                )
+                              })
+                            }
+                          </td>
 
                           <td>{movie.popularity}</td>
                           <td>{movie.vote_average}</td>
